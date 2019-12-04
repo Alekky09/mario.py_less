0a3e85dff2f78425e6d0f993021f99ff018774b9
Alekky09 <Alekky09@users.noreply.github.com>
2019-12-04 23:39:09 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -1,7 +1,11 @@
 from cs50 import get_int
 
-h = get_int("Height: ")
 
-for i in range(h):
-    print(" " * (h - i - 1) + "#" * (i + 1))
+while True:
+
+    h = get_int("Height: ")
+    if (h > 0 and h < 9):
+        for i in range(h):
+            print(" " * (h - i - 1) + "#" * (i + 1))
+        break
 
